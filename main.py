f4dc5a5c29d994b5de31854f8ebe6538a1838d6b
Howrad <9yiwei@gmail.com>
2023-10-17 15:36:34 +0800

    Day 39: Storing extracted data in text file

--- main.py
+++ main.py
@@ -18,9 +18,32 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
+
+def store(extracted):
+    try:
+        with open('data.txt', 'a') as file:
+            file.write(extracted + '\n')
+    except:
+        with open('data.txt', 'w') as file:
+            file.write(extracted + '\n')
+
+
+def read():
+    try:
+        with open('data.txt', 'r') as file:
+            return file.read()
+    except:
+        with open('data.txt', 'w') as file:
+            pass
+
+
 scraped = scrape(URL)
 extracted = extract(scraped)
+content = read()
+
 print(extracted)
 
-
+if extracted != "No upcoming tours":
+    if extracted not in content:
+        store(extracted)
 
